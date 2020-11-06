cd54d4ed86f93a6d02e404c8ed20d4707c6f2b89
Jairo Silva <70125199+jairosilva2005@users.noreply.github.com>
2020-11-06 20:01:31 -0300

    Rename questão9.cpp to media_de_turma_normal.cpp

--- media_de_turma_normal.cpp
+++ media_de_turma_normal.cpp
@@ -10,19 +10,19 @@ int main () {
       system("cls");
       double nota1, nota2, nota3, media[qntalunos], total = 0;
       for (z = 1 ; z <= qntalunos ; z++) {
-      	cout << "Digite a primeira nota do " << z << "º aluno: " << endl;
+      	cout << "Digite a primeira nota do " << z << "Âº aluno: " << endl;
       	cin >> nota1;
-      	cout << "Digite a segunda nota do " << z << "º aluno: " << endl;
+      	cout << "Digite a segunda nota do " << z << "Âº aluno: " << endl;
       	cin >> nota2;
-      	cout << "Digite a terceira nota do " << z << "º aluno: " << endl;
+      	cout << "Digite a terceira nota do " << z << "Âº aluno: " << endl;
       	cin >> nota3;
       	media[z]= (nota1 + nota2 + nota3)/3;
       	total = total+media[z];
-      	cout << "A média do aluno é: " << media[z] << "" << endl;
+      	cout << "A mÃ©dia do aluno Ã©: " << media[z] << "" << endl;
       	system("pause");
       	system("cls");
 	  }
-	cout << "A média total dos alunos é: " << total/qntalunos << endl;
+	cout << "A mÃ©dia total dos alunos Ã©: " << total/qntalunos << endl;
     system("pause");
  return 0;
  
